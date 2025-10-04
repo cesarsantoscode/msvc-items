92b89492fe92210c774d5741deaf9bc35aeafcdf
César Santos <cesar.santos@ifnewcode.com>
2025-10-04 17:48:11 -0500

    WebClient Implement

--- src/main/java/com/cesar/springcloud/msvc_items/controller/ItemController.java
+++ src/main/java/com/cesar/springcloud/msvc_items/controller/ItemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.cesar.springcloud.msvc_items.service.ItemService;
 public class ItemController {
 
     @Autowired
+    @Qualifier("itemWCServiceImpl")
     private ItemService itemService;
 
     @GetMapping
---
+++ src/main/java/com/cesar/springcloud/msvc_items/service/impl/ItemWCServiceImpl.java
@@ -0,0 +1,54 @@
+package com.cesar.springcloud.msvc_items.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.cesar.springcloud.msvc_items.model.Item;
+import com.cesar.springcloud.msvc_items.model.Product;
+import com.cesar.springcloud.msvc_items.service.ItemService;
+
+//@Primary
+@Service
+public class ItemWCServiceImpl implements ItemService {
+
+    @Autowired
+    private WebClient.Builder webClient;
+
+    @Override
+    public List<Item> findAll() {
+        return webClient.build()
+            .get()
+            .uri("http://msvc-products/api/products")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToFlux(Product.class)
+            .map(product -> new Item(product, new Random().nextInt(10)))
+            .collectList()
+            .block();
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Map<String, Long> params = new HashMap<>();
+        params.put("id", id);
+        return Optional.ofNullable(webClient.build()
+            .get()
+            .uri("http://msvc-products/api/products/{id}", params)
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(Product.class)
+            .map(product -> new Item(product, new Random().nextInt(10)))
+            .block()
+        );
+    }
+
+}
